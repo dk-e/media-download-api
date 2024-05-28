adca660b7fed6d61f18ab6513be44ce91a8c3a3c
Damon Windsor <damonwindsor@gmail.com>
2024-05-29 02:15:32 +1000

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
 
 const corsOptions = {
-    origin: ['http://localhost:5173', '*'],
+    origin: ['http://localhost:5173', '*', 'https://www.linkify.gg'],
     credentials: true,
     optionsSuccessStatus: 200,
 };
