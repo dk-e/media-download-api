bcb7d8f977013956d33df3f1d56f126d458f8891
Damon Windsor <damonwindsor@gmail.com>
2024-05-29 14:07:06 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,20 @@
-const express = require('express')
-const cors = require('cors')
-
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
-const corsOptions = {
-    origin: "*",
-    credentials: true,
-    optionsSuccessStatus: 200,
-};
+app.use(cors()); // Enable CORS for all routes
 
-// app.use(cors(corsOptions))
-app.use(cors({ origin: "*" })) // default
-
-app.use((req, res, next) => {
-    res.setHeader(
-        "Access-Control-Allow-Origin",
-        "*"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "POST, GET, OPTIONS"
-    )
-})
+// Handle preflight requests
+app.options('*', cors());
 
 const youtubeRoutes = require("./api/youtube.js");
+app.use("/youtube", youtubeRoutes);
 
-app.use("/youtube", youtubeRoutes)
-
-
-const port = 5000
-app.listen(5000, function () {
-    console.log(`Server started on port: ${port}`)
-})
+const port = 5000;
+app.listen(port, function () {
+    console.log(`Server started on port: ${port}`);
+});
