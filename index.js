b8828e6a3968a39d1a1ddde862cbb163e5fdad52
Damon Windsor <damonwindsor@gmail.com>
2024-05-29 12:38:07 +1000

    setHeaders

--- index.js
+++ index.js
@@ -16,6 +16,20 @@ const corsOptions = {
 // app.use(cors(corsOptions))
 app.use(cors({ origin: "*" })) // default
 
+app.use((req, res, next) => {
+    res.setHeader(
+        "Access-Control-Allow-Origin"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "POST, GET, OPTIONS"
+    )
+})
+
 const youtubeRoutes = require("./api/youtube.js");
 
 app.use("/youtube", youtubeRoutes)
