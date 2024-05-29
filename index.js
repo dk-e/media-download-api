8528ad4707106c9664bd3ea7fd290de48e9eb18a
Damon Windsor <damonwindsor@gmail.com>
2024-05-29 14:01:22 +1000

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors({ origin: "*" })) // default
 app.use((req, res, next) => {
     res.setHeader(
         "Access-Control-Allow-Origin",
-        accessControlAllowOriginValue
+        "*"
     );
     res.setHeader(
         "Access-Control-Allow-Headers",
