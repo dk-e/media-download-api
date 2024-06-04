e16e920bcefd52a8a0f054673431f089ac0f3d71
Damon Windsor <damonwindsor@gmail.com>
2024-06-05 02:09:03 +1000

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ const morgan = require('morgan')
 
 const app = express();
 
-app.use(cors({ 
-    origin: "https://linkify.gg", // Allow requests only from this origin
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,6 +11,8 @@ app.use(morgan('dev'));
 
 const youtubeRoutes = require("./api/youtube.js");
 app.use("/youtube", youtubeRoutes);
+
+app.use(cors());
 
 const port = 5000;
 app.listen(port, function () {
