9387ab5cf4a75841c1c5de003237e0d255e557b1
Damon Windsor <damonwindsor@gmail.com>
2024-06-05 15:11:09 +1000

    Update index.js

--- index.js
+++ index.js
@@ -24,11 +24,6 @@ if (!fs.existsSync(tempDir)) {
 const youtubeRoutes = require("./api/youtube.js");
 app.use("/youtube", youtubeRoutes);
 
-// Serve the React app's HTML file for all routes
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
 const port = process.env.PORT || 5000;
 app.listen(port, function () {
     console.log(`Server started on port: ${port}`);
