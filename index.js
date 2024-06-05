67aaff571cc6a9778580974c51067e63743122a0
Damon Windsor <damonwindsor@gmail.com>
2024-06-05 13:05:08 +1000

    ensure temp directory exists

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan')
+const fs = require('fs')
+const path = require('path')
 
 const app = express();
 
@@ -11,6 +13,13 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+
+// Ensure the existence of the temp directory
+const tempDir = path.join(__dirname, 'temp');
+if (!fs.existsSync(tempDir)) {
+    fs.mkdirSync(tempDir);
+    console.log('Temp directory created successfully.');
+}
 
 const youtubeRoutes = require("./api/youtube.js");
 app.use("/youtube", youtubeRoutes);
