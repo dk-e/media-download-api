e4a2b68450316a26e34ee24fdf305394ab6474f0
Damon Windsor <damonwindsor@gmail.com>
2024-06-05 14:58:46 +1000

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const cors = require('cors');
 const morgan = require('morgan')
 const fs = require('fs')
 const path = require('path')
-const httpProxy = require('http-proxy')
 
 const app = express();
-const proxy = httpProxy.createProxyServer();
 
 app.use(cors({ 
     origin: "*",
@@ -26,12 +24,12 @@ if (!fs.existsSync(tempDir)) {
 const youtubeRoutes = require("./api/youtube.js");
 app.use("/youtube", youtubeRoutes);
 
-// Define a route handler to proxy requests to the client-side application
+// Serve the React app's HTML file for all routes
 app.get('*', (req, res) => {
-    proxy.web(req, res, { target: 'https://linkify.gg/' });
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, function () {
     console.log(`Server started on port: ${port}`);
 });
